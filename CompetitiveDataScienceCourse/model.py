62d4ae04aa5bbbd22a0e03e5eb713fe2300e8e54
Matthew Ciaramitaro <mciama1996@gmail.com>
2018-07-30 23:03:57 -0400

    Submitted 2 solutions, best result from rbf trained on 500,000 random datapoints

--- CompetitiveDataScienceCourse/model.py
+++ CompetitiveDataScienceCourse/model.py
@@ -4,15 +4,18 @@ import pandas as pd
 import random
 from sklearn.svm import SVR
 from sklearn.model_selection import GridSearchCV
+import pickle
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn import linear_model
 
 DATA_DIR = "~/.kaggle/competitions/competitive-data-science-final-project/"
 
 # TODO:
 # read in csv file, remove headers and date, sort by date_block_num by item_id by store_id with custom sort criteria
 # aggregate variable item_cnt_day in last column for each store in date_block_num as new row item_cnt_month
-train_set = pd.read_csv(DATA_DIR + "sales_train.csv")  
-train_set=train_set.iloc[: , 1:]
-train_set = train_set.sort_values(by=["date_block_num", "shop_id","item_id"])
+#train_set = pd.read_csv(DATA_DIR + "sales_train.csv")  
+#train_set=train_set.iloc[: , 1:]
+#train_set = train_set.sort_values(by=["date_block_num", "shop_id","item_id"])
 #train_set = train_set.agg(["sum"])
 #print(train_set)
 
@@ -27,28 +30,53 @@ def agg_multiple(df, labels, aggvar, repl=None):
 
 #used for file creation
 #train_set = agg_multiple(train_set, ["date_block_num", "shop_id", "item_id"], "item_cnt_day", repl="item_cnt_mnth")
+#train_set[train_set["item_cnt_mnth"] > 20] = 20
 #train_set.to_csv(DATA_DIR + "train_data.csv")
 
 train_set = pd.read_csv(DATA_DIR + "train_data.csv")
+
+train_set = train_set.sample(100000) #I don't have time to train on 3mil
+
 X_ = np.array(train_set[["date_block_num", "shop_id","item_id"]])
 y_ = np.array(train_set[["item_cnt_mnth"]])
-X_test = np.array(train_set.head(5000)[["date_block_num", "shop_id","item_id"]])
-y_test = np.array(train_set[["item_cnt_mnth"]].head(5000))
+
+tests = train_set.sample(5000)
+X_test = np.array(tests[["date_block_num", "shop_id","item_id"]])
+y_test = np.array(tests[["item_cnt_mnth"]])
 
 
+#regr = SVR(C=1, epsilon = .1, gamma=.001, kernel="sigmoid")
+#regr.fit(X_, y_.ravel())
 
-regr = SVR(C=1, epsilon = .1, gamma=.001, kernel="sigmoid")
+regr = SVR(C=50, epsilon = .1, gamma=.001, kernel="rbf")
 regr.fit(X_, y_.ravel())
-preds = np.round(regr.predict(X_test))
-print(regr.score(X_test, y_test.ravel()))
-#print(regr.score(X, y.ravel()))
 
-#true accuracy function
+preds = np.round(regr.predict(y_test))
+
 true = np.sum((preds == y_test.ravel() )) / len(y_test) 
 print("true accuracy is ", true)
 
 
 
+
+pickle.dump(regr, open("finalized_model.sav", 'wb'))
+
+
+
+file = "finalized_model.sav"
+with open(file, "rb") as f:
+    regr = pickle.load(f)
+
+print("reading in test set")
+data = pd.read_csv(DATA_DIR +"test.csv")
+data["date_block_num"] = 34
+X = np.array(data[["date_block_num", "shop_id","item_id"]])
+print("creating predictions")
+y_hat = np.round(regr.predict(X))
+data["item_cnt_month"] = y_hat
+print("generating submission file")
+subm = data[["ID", "item_cnt_month"]]
+subm.to_csv(DATA_DIR + "final.csv", index=False)
 #print("Hyper Parameter Tuning")
 
 #Par1 = {'kernel' : ['sigmoid', 'rbf'], 'C': [.001, .1, 100, 1000], 'epsilon':[.1], 'gamma': [.0001, .001, .01, .1]}
@@ -60,8 +88,6 @@ print("true accuracy is ", true)
 #print(clf1.best_estimator_)
 
 #preds = np.round(clf1.predict(X_test))
-#true = np.sum((preds == y_test.ravel() )) / len(y_test) 
-#print("true accuracy is ", true)
 
 
 
