8e3f9d1485635f4b535bf9dc30b17b9f9e61a845
Ubuntu <ubuntu@ip-172-31-71-118.ec2.internal>
2018-08-16 15:39:43 +0000

    multiple update problem

--- CompetitiveDataScienceCourse/model.py
+++ CompetitiveDataScienceCourse/model.py
@@ -11,7 +11,7 @@ from sklearn import linear_model
 DATA_DIR = "../"
 
 dataset = pd.read_csv(DATA_DIR + "train_data.csv")
-train_set = train_set.sample(frac=1) #I don't have time to train on 3mil
+train_set = dataset.sample(frac=1) #I don't have time to train on 3mil
 
 X_ = np.array(train_set[["date_block_num", "shop_id","item_id"]])
 y_ = np.array(train_set[["item_cnt_mnth"]])
